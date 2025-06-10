770a474506659b89b878a50b6692d34b04663f92
Pieklonidas <tymoteuszkarwackip@gmail.com>
2025-06-10 19:14:19 +0200

    Update main.rs

--- LearnRustInAMonthOfLunches/iterators_and_closures_2/src/main.rs
+++ LearnRustInAMonthOfLunches/iterators_and_closures_2/src/main.rs
@@ -1,4 +1,3 @@
-use core::num;
 use std::char;
 
 struct Company {
@@ -29,6 +28,12 @@ fn in_char_vec(char_vec: &Vec<char>, check: char) {
         "Is {check} inside? {}",
         char_vec.iter().any(|&char| char == check)
     );
+}
+
+#[derive(Debug)]
+struct CombinedEvents {
+    num_of_events: u32,
+    data: Vec<String>,
 }
 
 fn main() {
@@ -155,6 +160,98 @@ fn main() {
     println!("{:?}", num_vec.iter().find(|number| *number % 11 == 0));
     println!("{:?}", num_vec.iter().position(|number| *number % 11 == 0));
 
+    let even_odd_iter = ["even", "odd"].into_iter().cycle();
+
+    let even_odd_vec: Vec<(i32, &str)> = (0..=5)
+        .zip(even_odd_iter)
+        .collect();
+    println!("{:?}", even_odd_vec);
+
+    let ten_chars: Vec<char> = ('a'..).take(10).collect();
+    let skip_then_ten_chars: Vec<char> = ('a'..).skip(1300).take(10).collect();
+
+    println!("{ten_chars:?}");
+    println!("{skip_then_ten_chars:?}");
     
-    
+    let some_numbers = vec![9, 6, 9, 10, 11];
+
+    println!("{}", some_numbers
+        .iter()
+        .fold(0, |total_so_far, next_number| total_so_far + next_number)
+    );
+
+    let events = [
+        "Went to grocery store",
+        "Came home",
+        "Fed cat",
+        "Fed cat again"
+    ];
+
+    let empty_events = CombinedEvents {
+        num_of_events: 0,
+        data: vec![],
+    };
+
+    let combined_events = 
+        events
+                .iter()
+                .fold(empty_events, |mut total_events, next_event| {
+                    total_events.num_of_events += 1;
+                    total_events.data.push(next_event.to_string());
+                    total_events
+                });
+    println!("{combined_events:#?}");
+
+    let mut number_iter = [7, 8, 9, 10].into_iter();
+    let first_two = number_iter.by_ref().take(2).collect::<Vec<_>>();
+    let second_two = number_iter.take(2).collect::<Vec<_>>();
+
+    println!("{first_two:?}");
+    println!("{second_two:?}");
+
+    let num_vec = vec![1, 2, 3, 4, 5, 6, 7];
+    for chunk in num_vec.chunks(3) {
+        println!("{:?}", chunk);
+    }
+    println!();
+    for window in num_vec.windows(3) {
+        println!("{:?}", window);
+    }
+
+    let some_str = "Er ist noch nicht erklärt. Aber es gibt Krieg. Verlaß dich drauf.";
+    for (index, item) in some_str.match_indices(|c| c > 'z') {
+        println!("{item} at {index}");
+    }
+
+    for (index, item) in some_str.match_indices(". ") {
+        println!("'{item}' at index {index}");
+    }
+
+    let just_numbers = vec![1, 5, 100];
+    let mut number_iter = just_numbers.iter().peekable();
+
+    for _ in 0..3 {
+        println!("I love the number {}", number_iter.peek().unwrap());
+        println!("I really love the number {}", number_iter.peek().unwrap());
+        println!("{} is such a nice number", number_iter.peek().unwrap());
+        number_iter.next();
+    }
+
+    let mut my_number = dbg!(9);
+    dbg!(my_number += 10);
+    dbg!(my_number);
+    let new_vec = dbg!(vec![8, 9, 10]);
+    let double_vec = dbg!(new_vec.iter().map(|x| x*2).collect::<Vec<i32>>());
+    dbg!(double_vec);
+
+    let new_vec = vec![8, 9, 10];
+ 
+    let double_vec = new_vec
+        .iter()
+        .inspect(|first_item| println!("The item is {first_item}"))
+        .map(|x| x * 2)
+        .inspect(|next_item| println!("The it is: {next_item}"))
+        .collect::<Vec<i32>>();
+
+    dbg!(double_vec);
 }
